5776439339e401f33e27bd073d3b3aacfac7a188
jinnatul <morolswediu@gmail.com>
2020-12-05 14:44:17 +0600

    filter unique tweet

--- bot.js
+++ bot.js
@@ -20,10 +20,28 @@ const retweet = (searchTag) => {
   T.get('search/tweets', params, (srcErr, srcData, srcRes) => {
     const tweets = srcData.statuses;
     if (!srcErr) {
+      let tweetIDList = [];
+
       tweets.forEach((tweet) => {
-        const tweetID = tweet.id_str;
+        if (tweet.text.startsWith('RT @')) {
+          if (tweet.retweeted_status) {
+            tweetIDList.push(tweet.retweeted_status.id_str);
+          } else {
+            tweetIDList.push(tweet.id_str);
+          }
+        } else {
+          tweetIDList.push(tweet.id_str);
+        }
+      });
+
+      // Filter unique tweet IDs.
+      tweetIDList = tweetIDList.filter((value, index, self) => self.indexOf(value) === index);
+
+      // Post tweet
+      tweetIDList.forEach((tweetID) => {
         T.post('statuses/retweet/:id', { id: tweetID }, (rtErr, rtData, rtRes) => {
-          if (!rtErr) {
+          if (!rtErr || rtData) {
+            console.log(rtData);
             console.log(`\n\nRetweeted! ID - ${tweetID}`);
           } else {
             console.log(`\nError... Duplication maybe...  ${tweetID}`);
